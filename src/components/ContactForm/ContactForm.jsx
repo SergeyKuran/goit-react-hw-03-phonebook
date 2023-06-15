e59b2b033710c5b67c1db9557f27f953b746c98b
Sergey Kuran <kuran.serg@gmail.com>
2023-06-16 01:27:59 +0300

    Edit pattern on input

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -46,7 +46,7 @@ class ContactForm extends Component {
             type="text"
             name="name"
             value={name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[A-Za-z\u0080-\uFFFF ']+$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             onChange={this.handleChange}
@@ -59,7 +59,7 @@ class ContactForm extends Component {
             type="tel"
             name="number"
             value={number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="^(\+?[0-9.\(\)\-\s]*)$"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             onChange={this.handleChange}
